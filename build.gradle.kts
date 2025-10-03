536001ed53fe46dc586ab56c4185dae7cec527a1
Stefano <34969+abaddon@users.noreply.github.com>
2025-10-03 11:28:57 +0100

    Update Maven repository URL and enhance release workflow with version printing

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,13 +35,13 @@ version = if(details.isCleanTag) lastTag else snapshotTag()
 
 repositories {
     mavenCentral()
-    mavenLocal()
     maven {
-        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
         mavenContent {
             snapshotsOnly()
         }
     }
+    mavenLocal()
 }
 
 dependencies {
